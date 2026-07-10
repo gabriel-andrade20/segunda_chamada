42d6a5a9ca8c86dc5ad6246e9cb30629cd184093
gabriel-andrade20 <gabrieltel095@gmail.com>
2026-07-09 21:48:49 -0300

    Delete database (1).py

--- database (1).py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import os
-
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-
-client = MongoClient(MONGO_URL)
-db = client["ecommerce"]
-colecao_pedidos = db["pedidos"]
